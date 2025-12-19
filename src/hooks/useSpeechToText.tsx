fca08f25e51c3601e644fa26194447ee2610c8e0
OriHaguel <orihaguel@gmail.com>
2025-12-19 11:59:05 +0200

    Try

--- src/hooks/useSpeechToText.tsx
+++ src/hooks/useSpeechToText.tsx
@@ -63,13 +63,11 @@ export function useSpeechToText() {
             };
 
             sr.onresult = (event) => {
-                let finalTranscript = '';
-                for (let i = event.resultIndex; i < event.results.length; i++) {
-                    if (event.results[i].isFinal) {
-                        finalTranscript += event.results[i][0].transcript;
-                    }
+                let transcriptSoFar = '';
+                for (let i = 0; i < event.results.length; i++) {
+                    transcriptSoFar += event.results[i][0].transcript;
                 }
-                setTranscript((prev) => prev + finalTranscript);
+                setTranscript(transcriptSoFar);
             };
 
             sr.onerror = (event) => {
